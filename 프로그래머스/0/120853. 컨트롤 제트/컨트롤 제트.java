69c31f8c6f5b5fde2380462677aea05be32d63d6
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2024-05-01 20:08:05 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.30 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -1,20 +1,40 @@
+import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        String[] splitted = s.split(" ");
+        Stack<Integer> stack = new Stack<>();
         
-        int previous = 0;
-        for (String str: splitted){
+        for(String str: s.split(" ")){
             if (str.equals("Z")){
-                answer -= previous;
+                stack.pop();
+            } else {
+                stack.push(Integer.valueOf(str));
             }
-            else {
-                answer += Integer.valueOf(str);
-                previous = Integer.valueOf(str);
-            }
-            
         }
         
+        for(int i: stack) {
+            answer += i;
+        }
+        
+        
         return answer;
+        
+        
+//         int answer = 0;
+//         String[] splitted = s.split(" ");
+        
+//         int previous = 0;
+//         for (String str: splitted){
+//             if (str.equals("Z")){
+//                 answer -= previous;
+//             }
+//             else {
+//                 answer += Integer.valueOf(str);
+//                 previous = Integer.valueOf(str);
+//             }
+            
+//         }
+        
+//         return answer;
     }
 }
