5a519cfb0dea6fa06580d6fe30776b785c1c67fb
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2024-05-10 11:55:23 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.08 ms, Memory: 76.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(long n) {
+        long i = (long) Math.sqrt(n);
+        if(i * i == n){
+            return (i+1) * (i+1);
+        } else {
+            return -1;
+        }
+    }
+}
