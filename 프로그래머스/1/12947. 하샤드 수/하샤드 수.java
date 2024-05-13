1480733c3f47b2bdea72490be45f5bdd86ed9615
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2024-05-13 09:28:44 +0900

    [level 1] Title: 하샤드 수, Time: 6.88 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.java
@@ -1,12 +1,11 @@
 class Solution {
     public boolean solution(int x) {
-        boolean answer = true;
         String str = String.valueOf(x);
         int sum = 0;
         for(int i = 0; i < str.length(); i++){
             sum += Integer.parseInt(str.charAt(i)+"");
         }
         
-        return x % sum == 0 ? true : false;
+        return x % sum == 0;
     }
 }
