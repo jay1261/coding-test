335ff268a47ef818ffd559e2121c4ceb28a98035
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2024-09-11 16:37:30 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 13.43 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,24 +1,18 @@
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-
 class Solution {
     public String[] solution(String[] strings, int n) {
-        Map<String, String> map = new TreeMap<>();
-        Arrays.sort(strings);
+        String[] answer = new String[strings.length];
 
-        for(String s: strings){
-            map.put(s, s.charAt(n)+"");
+        for(int i = 0; i < strings.length; i++){
+            answer[i] = strings[i].charAt(n) + strings[i];
         }
         
-        String[] answer = new String[strings.length];
-        AtomicInteger index = new AtomicInteger(0);
-
-        map.entrySet()
-            .stream()
-            .sorted(Map.Entry.comparingByValue())
-            .forEach(i -> answer[index.getAndIncrement()] = i.getKey());
+        Arrays.sort(answer);
         
-                
+        for(int i = 0; i < strings.length; i++){
+            answer[i] = answer[i].substring(1);
+        }
+        
         return answer;
     }
 }
