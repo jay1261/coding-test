72141e5fb872d98427b0b039121bfb869858af7e
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2024-09-20 16:45:50 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.13 ms, Memory: 79.8 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,20 +1,18 @@
 class Solution {
     public int solution(int n) {
+               
         StringBuilder sb = new StringBuilder();
         
         int answer = 0;
-        int current = n;
         
-        while(current > 0){
-            if(current % 3 > 10){
-                sb.append(current % 3);
-            } else {
-                sb.append((current % 3));
-            }
-            current /= 3;
+        while(n > 0){
+            sb.append(n % 3);
+            n /= 3;
         }
+        
         char[] cArr = sb.toString().toCharArray();
         int length = cArr.length;
+        
         for(int i = 0; i < length; i++){
             answer += ((int)cArr[i] - 48) * Math.pow(3, length - i - 1);
         }
