07e84b3c59260b7672605503c8bc64dcc9e36802
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2024-09-23 17:43:03 +0900

    [level 1] Title: 소수 찾기, Time: 29.07 ms, Memory: 53.2 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -1,20 +1,46 @@
+import java.util.*;
 class Solution {
     public int solution(int n) {
-        int answer = 0;
-        for(int i = 2; i <= n; i++){
-            boolean temp = true;
-            for(int j = 2; j <= Math.sqrt(i); j++){
-                if(i % j == 0){
-                    temp = false;
-                    // System.out.println("(" + i + "," + j+")");
-                    break;
+        boolean[] isPrime = new boolean[n+1];
+        Arrays.fill(isPrime, true);
+        
+        isPrime[0] = false;
+        isPrime[1] = false;
+        
+        
+        for(int i = 2; i*i <= n; i++){
+            if(isPrime[i]){
+                for(int j = i*i; j <= n; j += i){
+                    isPrime[j] = false;
                 }
             }
-            if (temp){
-                // System.out.println(i);
+        }
+        
+        int answer = 0;
+        
+        for(int i = 0; i < isPrime.length; i++){
+            if(isPrime[i]){
                 answer++;
             }
         }
+        
         return answer;
+        
+        // int answer = 0;
+        // for(int i = 2; i <= n; i++){
+        //     boolean temp = true;
+        //     for(int j = 2; j <= Math.sqrt(i); j++){
+        //         if(i % j == 0){
+        //             temp = false;
+        //             // System.out.println("(" + i + "," + j+")");
+        //             break;
+        //         }
+        //     }
+        //     if (temp){
+        //         // System.out.println(i);
+        //         answer++;
+        //     }
+        // }
+        // return answer;
     }
 }
