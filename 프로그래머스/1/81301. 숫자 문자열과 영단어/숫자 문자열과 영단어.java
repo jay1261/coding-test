fe4549afdef5227465670d9f39393c6a3bdd7b04
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2024-09-23 11:35:53 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 13.78 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -2,26 +2,34 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-        ArrayList<String> alpha = new ArrayList<>(Arrays.asList("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"));
-        String tmp = "";
-        String answer = "";
+        String[] alpha =  {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
         
-        for(int i = 0; i < s.length(); i++){
-            
-            
-            if(s.charAt(i) < 65){
-                answer += s.charAt(i);
-            } else {
-                tmp += s.charAt(i);
-            }
-            
-            if(alpha.contains(tmp)){
-                answer += alpha.indexOf(tmp);
-                tmp = "";
-                System.out.println(tmp);
-            }
+        for(int i = 0; i < alpha.length; i++){
+            s = s.replace(alpha[i], i+"");
         }
         
-        return Integer.valueOf(answer);
+        return Integer.valueOf(s);
     }
+//         ArrayList<String> alpha = new ArrayList<>(Arrays.asList("zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"));
+//         String tmp = "";
+//         String answer = "";
+        
+//         for(int i = 0; i < s.length(); i++){
+            
+            
+//             if(s.charAt(i) < 65){
+//                 answer += s.charAt(i);
+//             } else {
+//                 tmp += s.charAt(i);
+//             }
+            
+//             if(alpha.contains(tmp)){
+//                 answer += alpha.indexOf(tmp);
+//                 tmp = "";
+//                 System.out.println(tmp);
+//             }
+//         }
+        
+//         return Integer.valueOf(answer);
+//     }
 }
