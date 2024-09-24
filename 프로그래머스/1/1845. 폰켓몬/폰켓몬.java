ac10b899588d59b92656e10e0dea296673fcc319
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2024-09-24 18:22:35 +0900

    [level 1] Title: 폰켓몬, Time: 45.19 ms, Memory: 88.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -2,15 +2,29 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
         int maxCount = nums.length / 2;
         int count = 0;
+        
         for(int i = 0; i < nums.length; i++){
-            if(!map.containsKey(nums[i]) && count < maxCount){
-                map.put(nums[i], 1);
+            if(!list.contains(nums[i]) && count < maxCount){
+                list.add(nums[i]);
                 count++;
             }
         }
-        return map.keySet().size();
+        
+        return list.size();
+        
+        
+        // Map<Integer,Integer> map = new HashMap<>();
+        // int maxCount = nums.length / 2;
+        // int count = 0;
+        // for(int i = 0; i < nums.length; i++){
+        //     if(!map.containsKey(nums[i]) && count < maxCount){
+        //         map.put(nums[i], 1);
+        //         count++;
+        //     }
+        // }
+        // return map.keySet().size();
     }
 }
