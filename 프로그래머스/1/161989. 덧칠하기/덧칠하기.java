2ab1cc29d53a28d8385842d4b248122e85158023
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2024-09-25 11:42:20 +0900

    [level 1] Title: 덧칠하기, Time: 1.39 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -1,17 +1,15 @@
 class Solution {
     public int solution(int n, int m, int[] section) {
-        int answer = 0;
-        
+        int answer = 1;
         int tmp = section[0];
+        
         for(int i = 1; i < section.length; i++){
-            if(section[i] < tmp + m) {
-                continue;
-            } else {
+            if(section[i] >= tmp + m) {
                 tmp = section[i];
                 answer++;
             }
         }
         
-        return answer+1;
+        return answer;
     }
 }
