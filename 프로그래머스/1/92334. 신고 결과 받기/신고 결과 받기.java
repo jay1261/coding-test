42366b9e00dd040ef4b65378bda0047d98c0cd6f
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2024-10-09 16:28:15 +0900

    [level 1] Title: 신고 결과 받기, Time: 257.89 ms, Memory: 158 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(String[] id_list, String[] report, int k) {
+        HashMap<String, HashSet<String>> reportMap = new HashMap<>();
+        HashMap<String, Integer> reported = new HashMap<>();
+        // 요청 중복 제외 
+        for(String str: report){
+            String[] splited = str.split(" ");
+            HashSet<String> set = reportMap.getOrDefault(splited[0], new HashSet<String>());
+            set.add(splited[1]);
+            reportMap.put(splited[0], set);
+        }
+        
+        // System.out.println(reportMap.toString());
+        
+        for(HashSet<String> set: reportMap.values()){
+            ArrayList<String> list = new ArrayList<String>(set);
+            for(String reportedUser: list){
+                reported.put(reportedUser, reported.getOrDefault(reportedUser, 0) + 1);
+            }
+        }
+        
+        // System.out.println(reported.toString());
+        
+        int[] answer = new int[id_list.length];
+        for(int i = 0; i < id_list.length; i++){
+            HashSet<String> set = reportMap.getOrDefault(id_list[i], new HashSet<>());
+            ArrayList<String> list = new ArrayList<String>(set);
+            for(String reportedUser: list){
+                if(reported.get(reportedUser) >= k){
+                    answer[i]++;
+                }
+            }
+        }
+        
+        
+        
+        return answer;
+    }
+}
