bd4bcf9a69addb4716dde058409d7a8ec11dafbd
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2024-12-10 09:14:12 +0900

    [Bronze V] Title: 아스키 코드, Time: 168 ms, Memory: 17684 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String a = sc.nextLine();
+        char c = a.charAt(0);
+
+        System.out.println((int) c);
+    }
+}
