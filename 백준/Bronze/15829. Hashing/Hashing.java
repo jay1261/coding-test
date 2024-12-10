70ea2051ce7a376e06114f50df4a978ecd98d6d5
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2024-12-10 17:50:03 +0900

    [Bronze II] Title: Hashing, Time: 184 ms, Memory: 17744 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -3,20 +3,23 @@ import java.util.*;
 class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        int r = 31;
-        int m = 1234567891;
+        long r = 31;
+        long m = 1234567891;
         
         int n = sc.nextInt();
         sc.nextLine();
         String str = sc.nextLine();
         
-        int sum = 0;
+        long sum = 0;
+        long power = 1;
         for(int i = 0; i < n; i++){
             char c = str.charAt(i);
             int num = (int)c - 96;
-            sum += num * Math.pow(r, i);
+            sum = (sum + (num * power) % m) % m;
+            
+            power = (power * r) % m;
         }
         
-        System.out.println(sum % m);
+        System.out.println(sum);
     }
 }
