8fc2567cf0248233338a6ab8f31d72ab585d5f20
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2025-01-06 10:21:04 +0900

    [Bronze I] Title: 진법 변환 2, Time: 180 ms, Memory: 17796 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -1,33 +1,29 @@
 import java.util.*;
 
-class Main{
+class Main{    
     public static void main(String[] args){
-
         Scanner sc = new Scanner(System.in);
         HashMap<Integer, Character> map = new HashMap<>();
 
-        int num = 10;
-        char c = 'A';
-
-        for(int i = 0; i < 10; i++){
-            map.put(i, (char)(48+i));
-        }
-        for(int i = 0; i <= 25; i++){
-            map.put(num+i, (char) (c+i));
-        }
-
         int n = sc.nextInt();
         int b = sc.nextInt();
-        
+
+        for(int i = 0; i < 10; i++){
+            map.put(i, (char) ('0'+ i));
+        }
+        for(int i = 10; i < 36; i++){
+            map.put(i, (char) ('A' + i - 10));
+        }
+
         StringBuilder sb = new StringBuilder();
 
-        while(n != 0){
+        while(n > 0){
             int mod = n % b;
             n = n / b;
-            
+
             sb.insert(0, map.get(mod));
         }
+
         System.out.println(sb.toString());
     }
-  
 }
