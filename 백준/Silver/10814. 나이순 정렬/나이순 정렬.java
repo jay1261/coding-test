4cf58e5351b87c3f69be2d5bb88ecc56ae8e819d
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2025-01-07 14:54:45 +0900

    [Silver V] Title: 나이순 정렬, Time: 1080 ms, Memory: 208304 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,23 +1,34 @@
+
 import java.util.*;
 
 public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        HashMap<Integer, ArrayList<String>> map = new HashMap<>();
+
         int n = sc.nextInt();
         sc.nextLine();
-        
-        String[][] arr = new String[n][2];
-        
         for(int i = 0; i < n; i++){
             String[] splited = sc.nextLine().split(" ");
-            arr[i][0] = splited[0];
-            arr[i][1] = splited[1];
+            int age = Integer.parseInt(splited[0]);
+            String name = splited[1];
+
+            ArrayList<String> list = map.getOrDefault(age, new ArrayList<>());
+            list.add(splited[1]);
+            map.put(age, list);
         }
-        
-        Arrays.sort(arr, (o1,o2) -> Integer.compare(Integer.parseInt(o1[0]), Integer.parseInt(o2[0])));
-        
-        for(String[] a: arr){
-            System.out.println(a[0] + " " + a[1]);
+
+        ArrayList<Integer> ageList = new ArrayList<>(map.keySet());
+        Collections.sort(ageList);
+
+        StringBuilder sb = new StringBuilder();
+        for(int age: ageList){
+            for(String name: map.get(age)){
+                sb.append(age).append(" ").append(name).append("\n");
+            }
         }
+
+        System.out.println(sb.toString());
     }
 }
+
