29b9056d2da7a605ddeb27283aa87e04b2d6ffb2
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2025-01-09 16:21:13 +0900

    [Silver IV] Title: 문자열 집합, Time: 360 ms, Memory: 30808 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,25 +1,47 @@
+import java.io.*;
 import java.util.*;
 
-class Main{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
-        HashSet<String> set = new HashSet<>();
-        
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        sc.nextLine();
-        
-        for(int i = 0; i < n; i++){
-            set.add(sc.nextLine());
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        String[] arr = new String[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = br.readLine();
         }
-        
+        Arrays.sort(arr);
+
         int answer = 0;
-        for(int i = 0; i < m; i++){
-            if(set.contains(sc.nextLine())){
+        for (int i = 0; i < m; i++) {
+            String target = br.readLine();
+            if (search(arr, target, 0, n - 1)) {
                 answer++;
             }
         }
-        
+
         System.out.println(answer);
-	}
+    }
+
+    public static boolean search(String[] arr, String target, int p, int q) {
+        if (q < p) {
+            return false;
+        }
+        
+        int mid = (p + q) / 2;
+        int compare = target.compareTo(arr[mid]);
+        
+        if (target.equals(arr[mid])) {
+            return true;
+        } else if (compare > 0) {
+            // 타겟보다 mid가 큰 경우
+            return search(arr, target, mid + 1, q);
+        } else {
+            // 타겟보다 mid가 작은 경우
+            return search(arr, target, p, mid - 1);
+        }
+    }
 }
