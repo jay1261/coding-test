41b0d3a1a36d2607f779b12432d2eeb49f191910
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2025-11-11 17:12:21 +0900

    [Bronze III] Title: 소금 폭탄, Time: 184 ms, Memory: 18220 KB -BaekjoonHub

--- 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
+++ 백준/Bronze/13223. 소금 폭탄/소금 폭탄.java
@@ -2,47 +2,60 @@ import java.util.*;
 
 class Main{
     public static void main(String[] args){
-
         Scanner sc = new Scanner(System.in);
-
-        String current = sc.nextLine();
+        String now = sc.nextLine();
         String target = sc.nextLine();
         
-        if(current.equals(target)){
+        String[] splitedN = now.split(":");
+        String[] splitedT = target.split(":");
+        
+        int nh = Integer.valueOf(splitedN[0]);
+        int nm = Integer.valueOf(splitedN[1]);
+        int ns = Integer.valueOf(splitedN[2]);
+        
+        int th = Integer.valueOf(splitedT[0]);
+        int tm = Integer.valueOf(splitedT[1]);
+        int ts = Integer.valueOf(splitedT[2]);
+        
+        StringBuilder sb = new StringBuilder();
+        
+        if(ts == ns && tm == nm && th == nh){
             System.out.println("24:00:00");
             return;
         }
 
-        int ch = Integer.valueOf(current.charAt(0) + "" +  current.charAt(1));
-        int cm = Integer.parseInt(current.charAt(3) + "" + current.charAt(4));
-        int cs = Integer.parseInt(current.charAt(6) + "" + current.charAt(7));
-
-        int th = Integer.parseInt(target.charAt(0) + "" + target.charAt(1));
-        int tm = Integer.parseInt(target.charAt(3) + "" + target.charAt(4));
-        int ts = Integer.parseInt(target.charAt(6) + "" + target.charAt(7));
-
-        
-        int as = ts - cs;
-        if(as < 0){
-            as += 60;
+        if(ts - ns < 0){
             tm--;
+            insert(sb, ts - ns + 60);
+        } else {
+            insert(sb, ts - ns);
         }
         
-        int am = tm - cm;
-        if(am < 0){
-            am += 60;
+        sb.insert(0, ":");
+        
+        if(tm - nm < 0){
             th--;
-        }
-                
-        int ah = th - ch;
-        if(ah < 0){
-            ah += 24;
+            insert(sb, tm - nm + 60);
+        } else {
+            insert(sb, tm - nm);
         }
         
-        String hh = ah < 10 ? "0"+ah : ah+"";
-        String mm = am < 10 ? "0"+am : am+"";
-        String ss = as < 10 ? "0"+as : as+""; 
-        System.out.println(hh+":"+mm+":"+ss);
+        sb.insert(0, ":");
+        
+        if(th - nh < 0){
+            insert(sb, th - nh + 24);
+        } else {
+            insert(sb, th - nh);
+        }
+        
+        System.out.println(sb.toString());
     }
-
+    
+    public static void insert(StringBuilder sb, int num){
+        if(num < 10) {
+            sb.insert(0, "0"+num);
+        } else {
+            sb.insert(0, num);
+        }
+    }
 }
