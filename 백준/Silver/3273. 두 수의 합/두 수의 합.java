25eee97d0f1057218361e1c9b07924ada77ab0f2
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2025-11-12 14:58:15 +0900

    [Silver III] Title: 두 수의 합, Time: 768 ms, Memory: 101832 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -2,36 +2,34 @@ import java.util.*;
 
 class Main{
     public static void main(String[] args){
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        for(int i = 0; i < n; i++){
-            arr[i] = sc.nextInt();
-        }
-        int x = sc.nextInt();
-
-        Arrays.sort(arr);
-
-        int count = 0;
-        int p = 0;
-        int q = arr.length-1;
         
-        while(p < q){
-            if(arr[p] + arr[q] == x){
-                p++;
-                q--;
-                count++;
-            } else if(arr[p] + arr[q] < x){
-                p++;
-            } else if(arr[p] + arr[q] > x){
-                q--;
+        for(int i = 0; i < n; i++){
+            int num = sc.nextInt();
+            arr[i] = num;
+        }
+        
+        int x = sc.nextInt();
+        Arrays.sort(arr);
+        
+        int answer = 0;
+        int left = 0;
+        int right = arr.length-1;
+        while(left < right){
+            int sum = arr[left] + arr[right];
+            if(sum == x){
+                answer++;
+                left++;
+                right--;
+            }else if(sum < x){
+                left++;
+            } else {
+                right--;
             }
         }
         
-        System.out.println(count);
+        System.out.println(answer);
     }
-
-   
-    
 }
