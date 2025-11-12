f82f010c71e72be49d78df01e01a4d7dee6f2ef5
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2025-11-12 16:12:52 +0900

    [Bronze I] Title: 유레카 이론, Time: 292 ms, Memory: 21128 KB -BaekjoonHub

--- 백준/Bronze/10448. 유레카 이론/유레카 이론.java
+++ 백준/Bronze/10448. 유레카 이론/유레카 이론.java
@@ -4,26 +4,28 @@ class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-
+        
         for(int i = 0; i < t; i++){
             int num = sc.nextInt();
-            int result = 0;
-            for(int j = 1; j < 45; j++){
-                for(int k = 1; k < 45; k++){
-                    for(int l = 1; l < 45; l++){
-                        if(getTn(j) + getTn(k) + getTn(l) == num){
-                            result = 1;
-                            break;
-                        };
-                    }   
-                }   
-            }
             
-            System.out.println(result);
+            System.out.println(solution(num));
         }
     }
-
-    public static int getTn(int n){
-        return n * (n+1) / 2;
+    
+    public static int solution(int num){
+        for(int i = 1; i <= 44; i++){
+            for(int j = 1; j <= 44; j++){
+                for(int k = 1; k <= 44; k++){
+                    if(triangleNum(i) + triangleNum(j) + triangleNum(k)==num){
+                        return 1; 
+                    }
+                }
+            }
+        }
+        return 0;
     }
+    
+    public static int triangleNum(int n){
+        return n * (n+1) / 2;
+    }   
 }
