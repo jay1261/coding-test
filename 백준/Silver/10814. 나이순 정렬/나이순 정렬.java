8238a016a1e099df809812820f5f439227b5fc8a
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2025-11-17 16:16:28 +0900

    [Silver V] Title: 나이순 정렬, Time: 1060 ms, Memory: 212864 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,34 +1,33 @@
-
 import java.util.*;
 
-public class Main {
+class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        HashMap<Integer, ArrayList<String>> map = new HashMap<>();
-
+        Map<Integer, List<String>> map = new HashMap<>();
         int n = sc.nextInt();
         sc.nextLine();
+        
         for(int i = 0; i < n; i++){
-            String[] splited = sc.nextLine().split(" ");
-            int age = Integer.parseInt(splited[0]);
-            String name = splited[1];
-
-            ArrayList<String> list = map.getOrDefault(age, new ArrayList<>());
-            list.add(splited[1]);
+            String[] inputs = sc.nextLine().split(" ");
+            int age = Integer.valueOf(inputs[0]);
+            String name = inputs[1];
+            
+            List<String> list = map.getOrDefault(age, new ArrayList<String>());
+            list.add(name);
             map.put(age, list);
         }
-
-        ArrayList<Integer> ageList = new ArrayList<>(map.keySet());
-        Collections.sort(ageList);
-
+        
+        List<Integer> list = new ArrayList<>(map.keySet());
+        list.sort((o1, o2) -> Integer.compare(o1,o2));
+        
         StringBuilder sb = new StringBuilder();
-        for(int age: ageList){
-            for(String name: map.get(age)){
-                sb.append(age).append(" ").append(name).append("\n");
+        for(int num: list){
+            List<String> sList = map.get(num);
+            for(String s: sList){
+                sb.append(num).append(" ").append(s).append("\n");
             }
         }
-
-        System.out.println(sb.toString());
+        
+        System.out.println(sb);
     }
 }
-
