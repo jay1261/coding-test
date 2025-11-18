4544db752c2e47e20223a66114b5c957440d7843
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2025-11-18 16:37:05 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 1184 ms, Memory: 308672 KB -BaekjoonHub

--- 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.java
@@ -1,37 +1,34 @@
 import java.util.*;
 
 class Main{
-	public static void main(String[] args){
-		Scanner sc = new Scanner(System.in);
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        sc.nextLine();
-        
-        String[] splited = sc.nextLine().split(" ");
-        int[] ground = new int[n+1];
-        int[] prefixGround = new int[n+2];
-        
-        for(int i = 1; i <= n; i++){
-            ground[i] = Integer.parseInt(splited[i-1]);
+        int[] arr = new int[n+1];
+        for(int i = 1; i <=n; i++){
+            arr[i] = sc.nextInt();
         }
         
+        int[] pSum = new int[n+2];
         for(int i = 0; i < m; i++){
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+            int start = sc.nextInt();
+            int end = sc.nextInt();
             int k = sc.nextInt();
             
-            prefixGround[a] += k;
-            prefixGround[b+1] -= k;
-        }
-        
-        for(int i = 1; i <= prefixGround.length-1; i++){
-            prefixGround[i] = prefixGround[i-1] + prefixGround[i];
+            pSum[start] += k;
+            pSum[end + 1] += k * -1;
         }
         
         StringBuilder sb = new StringBuilder();
-        for(int i = 1; i <= n; i++){
-            sb.append(ground[i] + prefixGround[i]).append(" ");
+
+        for(int i = 1; i < pSum.length-1; i++){
+            pSum[i] = pSum[i] + pSum[i-1];
+            
+            sb.append(arr[i] + pSum[i]).append(" ");
         }
-        System.out.println(sb.toString());
-	}
+        
+        System.out.println(sb);
+        
+    }
 }
