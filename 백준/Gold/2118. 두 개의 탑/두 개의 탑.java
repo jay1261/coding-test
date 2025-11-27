83578b7543706a9f63582d5d0720132afd24ce3e
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2025-11-27 18:09:07 +0900

    [Gold V] Title: 두 개의 탑, Time: 1820 ms, Memory: 70744 KB -BaekjoonHub

--- 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
+++ 백준/Gold/2118. 두 개의 탑/두 개의 탑.java
@@ -4,34 +4,34 @@ class Main{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        
         int[] arr = new int[n];
-        arr[0] = 0;
-        int max = 0;
+        int[] prefix = new int[n+1];
         
         for(int i = 1; i <= n; i++){
             int num = sc.nextInt();
-            
-            if(i == n){
-                max = arr[i-1] + num;
-                continue;
-            }
-            
-            arr[i] = arr[i-1] + num;
+            arr[i-1] = num;
+            prefix[i] = prefix[i-1] + num;
         }
         
-        // 0 1 3 6 10 15
-        
+        int left = 0;
+        int right = 0;
         int answer = 0;
         for(int i = 0; i < n; i++){
             for(int j = i+1; j < n; j++){
-                int right = arr[j] - arr[i];
-                int left = max - right;
-                int result = right < left ? right : left;
-                answer = Math.max(result, answer);
+                int rDistance = prefix[j] - prefix[i];
+                int lDistance = prefix[n] - (prefix[j] - prefix[i]);
+                int shortOne = Math.min(rDistance, lDistance);
+                
+                //System.out.println("i: " + i + ", j: " + j + ", rd: " + rDistance + ", ld: " + lDistance);
+                answer = Math.max(answer, shortOne);
             }
         }
         
+        //System.out.println(Arrays.toString(prefix));
         System.out.println(answer);
 	}
+
+    // . 1 . 2 . 3 . 4 . 5 ->
+    
+    // 1 3 6 10 15
 }
