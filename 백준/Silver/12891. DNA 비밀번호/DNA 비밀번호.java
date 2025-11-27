66a7b3d012f7ca65d7a45d14f61e92fb9449babc
이동재(Jay) <99792672+jay1261@users.noreply.github.com>
2025-11-27 17:31:15 +0900

    [Silver II] Title: DNA 비밀번호, Time: 500 ms, Memory: 33788 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,68 +1,82 @@
 import java.util.*;
 
 class Main{
-public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
         int s = sc.nextInt();
         int p = sc.nextInt();
         sc.nextLine();
-
         String input = sc.nextLine();
-
-        HashMap<Character, Integer> map = new HashMap<>();
-        map.put('A', sc.nextInt());
-        map.put('C', sc.nextInt());
-        map.put('G', sc.nextInt());
-        map.put('T', sc.nextInt());
+        //A C G T
+        int[] arr = new int[4];
         int sum = 0;
-        for(int num: map.values()){
+        for(int i = 0; i < 4; i++){
+            int num = sc.nextInt();
+            arr[i] = num;
             sum += num;
         }
-
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < p; i++){
-            char c = input.charAt(i);
-            sb.append(c);
-            int num = map.get(c);
-            if(num > 0){
-                sum--;
-            }
-            num--;
-
-            map.put(c, num);
-        }
-
         int answer = 0;
+        
+        //처음 subString
+        String subStr = input.substring(0, p);
+        for(char c: subStr.toCharArray()){
+            int index = getIndex(c);
+
+            if(index != -1){
+                if(arr[index] > 0) sum--;
+                arr[index]--;
+            }
+            
+        }
         if(sum == 0){
             answer++;
         }
 
-        for(int i = p; i < s; i++){
+        // 하나씩 추가하고 빼기
+        
+        for(int i = 0; i < s-p; i++){
             char c = input.charAt(i);
-            sb.append(c);
-
-            int num = map.get(c);
-            if(num > 0){
-                sum--;
+            int index = getIndex(c);
+            
+            if(index != -1){
+                arr[index]++;
+                if(arr[index] > 0){
+                    sum++;
+                }
             }
-            num--;
-            map.put(c, num);
-
-            char c2 = input.charAt(i-p);
-            sb.delete(0, 0);
-
-            int num2 = map.get(c2);
-            if(++num2 > 0){
-                sum++;
+            
+            char c2 = input.charAt(i+p);
+            int index2 = getIndex(c2);
+            
+            if(index2 != -1){
+                if(arr[index2] > 0){
+                    sum--;
+                }
+                arr[index2]--;
             }
-            map.put(c2, num2);
-
+            
             if(sum == 0){
                 answer++;
             }
         }
+        
+
 
         System.out.println(answer);
+	}
+    
+    public static int getIndex(char c){
+        switch (c){
+            case 'A':
+                return 0;
+            case 'C':
+                return 1;
+            case 'G':
+                return 2;
+            case 'T':
+                return 3;
+            default:
+                return -1;
+        }
     }
 }
